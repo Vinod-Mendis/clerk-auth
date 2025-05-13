f7355ac68fabcdc18cebb3f3972f784b5e040b20
Vinod Mendis <136561979+Vinod-Mendis@users.noreply.github.com>
2025-05-13 16:07:33 +0700

    init app

---
+++ src/app/sign-in/page.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+// app/sign-in/page.js
+import { SignIn } from "@clerk/nextjs";
+
+export default function SignInPage() {
+  return <SignIn />;
+}
---
+++ src/app/sign-up/page.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+// app/sign-up/page.js
+import { SignUp } from "@clerk/nextjs";
+
+export default function SignUpPage() {
+  return <SignUp />;
+}
